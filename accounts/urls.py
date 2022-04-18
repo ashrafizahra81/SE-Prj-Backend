e9ba776a3174fc022d16e62f74e7bd6ecac0ae7f
Mahla Shamsabadi <shamsabadimahla@gmail.com>
2022-04-18 19:54:56 +0430

    login added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
 from . import views
 
 
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.userRegister),
+    path('login/', obtain_auth_token, name= 'login'),
 ]
