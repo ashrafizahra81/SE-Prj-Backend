e164343ea9fb5f9c05a4bb4e6a14f3f98892a34b
Mahla Shamsabadi <shamsabadimahla@gmail.com>
2022-04-19 23:06:57 +0430

    authentication added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework.authtoken import views as auth_token
 from . import views
 
 
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegister.as_view()),
+    path('api-token-auth-/', auth_token.obtain_auth_token),
 ]
