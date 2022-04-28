6efc7bbb8f1df44d094c933eb7c650350304ec5f
m-moslemifar <m.r.moslemifar@gmail.com>
2022-04-28 20:29:31 +0430

    add more CORS properties

--- Backend/settings.py
+++ Backend/settings.py
@@ -61,6 +61,26 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Backend.urls'
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 TEMPLATES = [
     {
