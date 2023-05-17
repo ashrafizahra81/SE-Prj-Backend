fb61bb7fb25179ae2a0d51f4d97744aea810789a
ashrafizahra81 <ashrafi.zahra81@gmail.com>
2023-05-17 08:26:47 -0700

    some errors in tests for urls were solved

--- accounts/tests/tests_accounts_urls.py
+++ accounts/tests/tests_accounts_urls.py
@@ -7,7 +7,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_register_url(self):
 
         #Arrange
-        register_url = reverse('register')
+        register_url = reverse('accounts:register')
 
         #Assert
         self.assertEqual(resolve(register_url).func.view_class , UserRegister)
@@ -15,7 +15,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_login_url(self):
     
         #Arrange
-        login_url = reverse('token_obtain_pair')
+        login_url = reverse('accounts:token_obtain_pair')
 
         #Assert
         self.assertEqual(resolve(login_url).func.view_class , CustomTokenObtainPairView)
@@ -23,7 +23,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_edit_profile_url(self):
     
         #Arrange
-        edit_profile_url = reverse('edit-profile')
+        edit_profile_url = reverse('accounts:edit-profile')
 
         #Assert
         self.assertEqual(resolve(edit_profile_url).func.view_class , UserEditProfile)
@@ -31,7 +31,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_create_shop_url(self):
         
         #Arrange
-        create_shop_url = reverse('create_shop')
+        create_shop_url = reverse('accounts:create_shop')
 
         #Assert
         self.assertEqual(resolve(create_shop_url).func.view_class , ShopManagerRegister)
@@ -39,7 +39,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_edit_shop_url(self):
         
         #Arrange
-        edit_shop_url = reverse('edit_shop')
+        edit_shop_url = reverse('accounts:edit_shop')
 
         #Assert
         self.assertEqual(resolve(edit_shop_url).func.view_class , EditShop)
@@ -47,7 +47,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_show_user_info_url(self):
         
         #Arrange
-        show_user_info_url = reverse('show_user_info')
+        show_user_info_url = reverse('accounts:show_user_info')
 
         #Assert
         self.assertEqual(resolve(show_user_info_url).func.view_class , ShowUserInfo)
@@ -55,7 +55,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_token_verify_url(self):
         
         #Arrange
-        token_verify_url = reverse('token_verify')
+        token_verify_url = reverse('accounts:token_verify')
 
         #Assert
         self.assertEqual(resolve(token_verify_url).func.view_class , TokenVerifyView)
@@ -63,7 +63,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_show_score_url(self):
         
         #Arrange
-        show_score_url = reverse('show_score')
+        show_score_url = reverse('accounts:show_score')
 
         #Assert
         self.assertEqual(resolve(show_score_url).func.view_class , show_score)
@@ -71,15 +71,15 @@ class TestAccountsUrls(SimpleTestCase):
     def test_reset_password_url(self):
         
         #Arrange
-        reset_password_url = reverse('reset_password')
+        reset_password_url = reverse('accounts:reset_password')
 
         #Assert
-        self.assertEqual(resolve(reset_password_url).func.view_class , reset_password)
+        self.assertEqual(resolve(reset_password_url).func , reset_password)
 
     def test_recover_password_url(self):
         
         #Arrange
-        recover_password_url = reverse('recover_password')
+        recover_password_url = reverse('accounts:recover_password', args=(1,))
 
         #Assert
         self.assertEqual(resolve(recover_password_url).func.view_class , RecoverPassword)
@@ -87,7 +87,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_receive_password_url(self):
         
         #Arrange
-        receive_password_url = reverse('receive_password')
+        receive_password_url = reverse('accounts:receive_email')
 
         #Assert
         self.assertEqual(resolve(receive_password_url).func.view_class , ReceiveEmailForRecoverPassword)
@@ -95,7 +95,7 @@ class TestAccountsUrls(SimpleTestCase):
     def test_verify_email_url(self):
         
         #Arrange
-        verify_email_url = reverse('verify_email')
+        verify_email_url = reverse('accounts:verify_email')
 
         #Assert
         self.assertEqual(resolve(verify_email_url).func.view_class , verfyUserToResgister)
