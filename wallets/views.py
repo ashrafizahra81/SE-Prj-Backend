6aa67a7034079e37913ef7b785dedf28029c89bd
Mahla Shamsabadi <shamsabadimahla@gmail.com>
2023-05-18 11:36:37 +0430

    logs of wallets app added

--- wallets/views.py
+++ wallets/views.py
@@ -6,8 +6,9 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
+import logging
+logger = logging.getLogger("django")
 
-# Create your views here.
 
 class ChargeWallet(APIView):
     permission_classes = [IsAuthenticated, ]
@@ -16,68 +17,70 @@ class ChargeWallet(APIView):
         data2 = request.data
         data = {}
         if(data2['insert'] == '' or data2['insert'] < 0 or data2['insert'] == 0):
+            logger.warn('data entered is invalid')
             data={"message": "مقدار وارد شده قابل قبول نیست"}
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
         wallet.balance += float(data2['insert'])
         wallet.save()
         data['balance'] = wallet.balance
+        logger.info('amount of '+str(request.data['insert'])+' added to wallet of user '+str(request.user.id))
         return Response(data, status=status.HTTP_200_OK)
 
-class BuyFromWallet(APIView):
-    permission_classes = [IsAuthenticated, ]
-    def get(self , request):
-        AllUserShoppingCart_list = list(UserShoppingCart.objects.all().values())
-        UserShoppingCart_list = []
-        UserShoppingCartProductId_list = []
-        print(AllUserShoppingCart_list)
-        data = {}
-        totalPrice = 0
-        for userShoppingCart in AllUserShoppingCart_list:
-            if userShoppingCart['user_id'] == request.user.id:
-                if userShoppingCart['status'] == "not Accepted":
-                    UserShoppingCart_list.append(userShoppingCart)
-                    UserShoppingCartProductId_list.append(userShoppingCart['product_id'])
+# class BuyFromWallet(APIView):
+#     permission_classes = [IsAuthenticated, ]
+#     def get(self , request):
+#         AllUserShoppingCart_list = list(UserShoppingCart.objects.all().values())
+#         UserShoppingCart_list = []
+#         UserShoppingCartProductId_list = []
+#         print(AllUserShoppingCart_list)
+#         data = {}
+#         totalPrice = 0
+#         for userShoppingCart in AllUserShoppingCart_list:
+#             if userShoppingCart['user_id'] == request.user.id:
+#                 if userShoppingCart['status'] == "not Accepted":
+#                     UserShoppingCart_list.append(userShoppingCart)
+#                     UserShoppingCartProductId_list.append(userShoppingCart['product_id'])
         
 
         
-        print(len(UserShoppingCart_list))
-        print(len(UserShoppingCartProductId_list))
+#         print(len(UserShoppingCart_list))
+#         print(len(UserShoppingCartProductId_list))
 
-        products_list = list(Product.objects.all().values())
-        while len(UserShoppingCartProductId_list) > 0:
-            for product in products_list:
-                if UserShoppingCartProductId_list[0] == product['id']:
-                    totalPrice += product['product_price']
-                    UserShoppingCartProductId_list.pop(0)
-                    break
+#         products_list = list(Product.objects.all().values())
+#         while len(UserShoppingCartProductId_list) > 0:
+#             for product in products_list:
+#                 if UserShoppingCartProductId_list[0] == product['id']:
+#                     totalPrice += product['product_price']
+#                     UserShoppingCartProductId_list.pop(0)
+#                     break
 
-        print("total price*******************")
-        print(totalPrice)
-        wallet = Wallet.objects.get(user=request.user)
+#         print("total price*******************")
+#         print(totalPrice)
+#         wallet = Wallet.objects.get(user=request.user)
 
-        if totalPrice > 0: 
+#         if totalPrice > 0: 
 
-            if wallet.balance >= totalPrice:
-                wallet.balance -= totalPrice
-                data['status'] = 'done!'
-            # myList = UserShoppingCart.objects.get(user_id=request.user.id)
-            # print(lent(myList))
-                for userShoppingCart in AllUserShoppingCart_list:
-                    if userShoppingCart['user_id'] == request.user.id:
-                        if userShoppingCart['status'] == "not Accepted":
-                            UserShoppingCartById = UserShoppingCart.objects.get(id=userShoppingCart['id'])
-                            UserShoppingCartById.status = "Accepted"
-                            UserShoppingCartById.save()
+#             if wallet.balance >= totalPrice:
+#                 wallet.balance -= totalPrice
+#                 data['status'] = 'done!'
+#             # myList = UserShoppingCart.objects.get(user_id=request.user.id)
+#             # print(lent(myList))
+#                 for userShoppingCart in AllUserShoppingCart_list:
+#                     if userShoppingCart['user_id'] == request.user.id:
+#                         if userShoppingCart['status'] == "not Accepted":
+#                             UserShoppingCartById = UserShoppingCart.objects.get(id=userShoppingCart['id'])
+#                             UserShoppingCartById.status = "Accepted"
+#                             UserShoppingCartById.save()
 
-                wallet.save()
+#                 wallet.save()
 
-            else:
-                data['status'] = 'Your account balance is insufficient!'
+#             else:
+#                 data['status'] = 'Your account balance is insufficient!'
         
-        else:
-            data['status'] = "there are no items in your shopping cart"
+#         else:
+#             data['status'] = "there are no items in your shopping cart"
    
 
         
-        return Response(data, status=status.HTTP_200_OK)
+#         return Response(data, status=status.HTTP_200_OK)
 
