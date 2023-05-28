c7c51a216e403027b7480d1f5bd1bd9768c89edd
Mahla Shamsabadi <shamsabadimahla@gmail.com>
2023-05-28 12:29:00 +0430

    log settings changed

--- Backend/settings.py
+++ Backend/settings.py
@@ -339,24 +339,24 @@ HANDLERS = {
     "console_handler": {
         "class": "logging.StreamHandler",
         "formatter": "simple",
-    },
-    "my_handler": {
-        "class": "logging.handlers.RotatingFileHandler",
-        "filename": f"{BASE_DIR}/logs/blogthedata.log",
-        "mode": "a",
-        "encoding": "utf-8",
-        "formatter": "simple",
-        "backupCount": 5,
-        "maxBytes": 1024 * 1024 * 5,  # 5 MB
-    },
-    "my_handler_detailed": {
-        "class": "logging.handlers.RotatingFileHandler",
-        "filename": f"{BASE_DIR}/logs/blogthedata_detailed.log",
-        "mode": "a",
-        "formatter": "simple",
-        "backupCount": 5,
-        "maxBytes": 1024 * 1024 * 5,  # 5 MB
-    },
+    }
+    # "my_handler": {
+    #     "class": "logging.handlers.RotatingFileHandler",
+    #     "filename": f"{BASE_DIR}/logs/blogthedata.log",
+    #     "mode": "a",
+    #     "encoding": "utf-8",
+    #     "formatter": "simple",
+    #     "backupCount": 5,
+    #     "maxBytes": 1024 * 1024 * 5,  # 5 MB
+    # },
+    # "my_handler_detailed": {
+    #     "class": "logging.handlers.RotatingFileHandler",
+    #     "filename": f"{BASE_DIR}/logs/blogthedata_detailed.log",
+    #     "mode": "a",
+    #     "formatter": "simple",
+    #     "backupCount": 5,
+    #     "maxBytes": 1024 * 1024 * 5,  # 5 MB
+    # },
 }
 
 LOGGERS = (
